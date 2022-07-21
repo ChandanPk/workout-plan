69240ac574076de7d5844e27400c8debf8ea70e2
ChandanPk <chandanpk9@gmail.com>
2022-07-21 18:30:39 +0530

    Creates model & schema

--- controllers/workoutController.js
+++ controllers/workoutController.js
@@ -1,4 +1,5 @@
 const Workout = require('../models/Workout')
+const mongoose = require('mongoose')
 
 // Get workouts
 module.exports.get_workouts = async (req, res) => {
@@ -26,11 +27,36 @@ module.exports.addWorkout_post = async (req, res) => {
 // Get single workout
 module.exports.get_workout = async (req, res) => {
     const { id } = req.params
-    console.log(id)
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        console.log("error occured")
+        res.json({ "error": "no such workout" })
+        return
+    }
     try {
         const data = await Workout.findById(id);
         res.status(200).json(data)
     } catch (error) {
-        res.json({"error": "Oops! No workout found"})        
+        res.json({ "error": "Oops! No workout found" })
     }
+}
+
+// Delete workout
+module.exports.delete_workout = async (req, res) => {
+    const { id } = req.params
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        console.log("error occured")
+        res.json({ "error": "no such workout" })
+        return
+    }
+    try {
+        const data = await Workout.findByIdAndDelete({_id: id})
+        if(!data){
+            res.status(404).json("No such workout found to delete")
+        }
+    } catch (error) {
+        res.json({"error": error})        
+    }
+    
 }
