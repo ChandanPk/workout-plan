77378876031c5505a1d37d2f0b8b669847546ae4
ChandanPk <chandanpk9@gmail.com>
2023-03-23 14:47:03 +0530

    create docker image & setup db

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import { config } from "dotenv";
+import workoutRoutes from "./routes/Workout.js";
+import mongoose from "mongoose";
 
 // cors config
 config();
@@ -7,7 +9,20 @@ config();
 // express app
 const app = express();
 
+// Middlewares
+app.use(express.json());
+console.log(process.env.MONGODB_URI, "=+=+");
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("conneted to db successfully"))
+  .catch((err) => console.log(err));
+
 // Routes
+app.use("/api/workouts", workoutRoutes);
+
 app.get("/", (req, res) => {
   res.send("<h1>Home page</h1>");
 });
